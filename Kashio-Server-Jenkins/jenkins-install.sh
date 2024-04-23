eb27d7ae943bde8f5ada7d0a76ddf748fb2049b9
eduard <ejsotelo4@gmail.com>
2024-04-23 00:05:45 -0500

    actualización de java

--- Kashio-Server-Jenkins/jenkins-install.sh
+++ Kashio-Server-Jenkins/jenkins-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Install dnf
+sudo yum install epel-release -y
+sudo yum install DNF -y
+
 # install jenkins
-sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+    https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo dnf upgrade
+# Add required dependencies for the jenkins package
+sudo dnf install fontconfig java-17-openjdk
+sudo dnf install jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
