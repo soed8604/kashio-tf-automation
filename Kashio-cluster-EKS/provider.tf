0875b703a22af9ebd17904a45761b72caf93bede
eduard <ejsotelo4@gmail.com>
2024-04-22 23:34:56 -0500

    agregar region en provider

--- Kashio-cluster-EKS/provider.tf
+++ Kashio-cluster-EKS/provider.tf
@@ -3,7 +3,10 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      region = "us-east-1"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Cambia esto a tu región de AWS deseada
 }
